17c679feaefbaa46803e309979694f6e9641d467
vvegesna01 <kvegesna01@gmail.com>
2023-06-02 11:49:02 -0400

    migrating changes

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { FaYoutube, FaLinkedin, FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import Link from "next/link";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="flex flex-col items-center justify-between p-8 bg-white">
+      <div className="flex flex-col p-5">
+      </div>
+      <div className="flex space-x-4 text-localhost_text">
+        <div className="">
+          <Link href="https://www.youtube.com/channel/UC3jv-V2sKktWbd16ISmz2WQ">
+            <div className="group">
+              <FaYoutube
+                size="35"
+                className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100"
+              />
+              <span className="sr-only">YouTube</span>
+            </div>
+          </Link>
+        </div>
+        <div className="">
+          <Link href="https://www.linkedin.com/in/keerthana-vegesna/">
+            <div className="group">
+              <FaLinkedin
+                size="35"
+                className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100"
+              />
+              <span className="sr-only">LinkedIn</span>
+            </div>
+          </Link>
+        </div>
+        <div className="">
+          <Link href="https://github.com/">
+            <div className="group">
+              <FaGithub
+                size="35"
+                className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100"
+              />
+              <span className="sr-only">GitHub</span>
+            </div>
+          </Link>
+        </div>
+        <div className="">
+          <Link href="mailto:example@gmail.com">
+            <div className="group">
+              <SiGmail
+                size="35"
+                className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100"
+              />
+              <span className="sr-only">Gmail</span>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+import Footer from "../footer/footer";
+
+const Navbar: React.FC = () => {
+  return (
+    <main className="">
+      <div className="flex items-center justify-center min-h-auto bg-gray-200">
+        <div className="flex flex-col items-center m-20">
+		<a href="/">
+            <h1 className="text-localhost_text text-5xl font-large font-extrabold mb-4 transition duration-300 transform hover:-translate-y-1 hover:scale-100">
+              Keerthana Vegesna
+            </h1>
+          </a>
+
+          <ul className="flex space-x-4">
+            <li className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100">
+              <a href="/about">ABOUT</a>
+            </li>
+            <li className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100">
+              <a href="/projects">PROJECTS</a>
+            </li>
+            <li className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100">
+              <a href="/experience">EXPERIENCE</a>
+            </li>
+            <li className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100">
+              <a href="/bookshelf">BOOKSHELF</a>
+            </li>
+            <li className="text-lg text-localhost_text hover:text-gray-500 transition duration-300 transform hover:-translate-y-1 hover:scale-100">
+              <a href="/resume">RESUME</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Navbar;
